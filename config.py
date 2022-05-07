5ddb101ce722129d0376adffc4b35948f940d7d7
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-08 03:36:27 +0400

    create config file separately

---
+++ config.py
@@ -0,0 +1,11 @@
+class SEGMENTS:
+    fill_color = "rgba(255, 165, 0, 0.3)",
+    stroke_width = 3
+    stroke_color = "red"
+    fill = (255, 178, 102, 100)
+
+
+class KEYPOINT:
+    radius = 10
+    fill = "red"
+    outline = "red"
