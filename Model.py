b5c83d64631881b327e5296611f4cd29f796424b
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-09 16:15:11 +0400

    create model class

---
+++ Model.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+class Model:
+    def __init__(self):
+        self.x = 10
+        self.y = 10
+
+    def predict(self):
+        self.x += randint(-2, 10)
+        self.y += randint(-2, 7)
+        return self.x, self.y
