d42913bb8e7cb9872f2b869f1fe5ed0d4aeb5771
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-12 01:44:27 +0400

    add bar plot for number of crossing

--- scripts/Analyzer.py
+++ scripts/Analyzer.py
@@ -68,30 +68,26 @@ class Analyzer:
             # Compare radius of circle with distance of its center from given point
             is_in_segment = (x - circle_x) ** 2 + (y - circle_y) ** 2 <= rad ** 2
 
-        in_segment = sum(is_in_segment)
-        return in_segment
+        # in_segment = sum(is_in_segment)
+        return is_in_segment
 
-    def count_elapsed_time_in_segments(self, predictions):
+    def _count_elapsed_time_in_segments(self, predictions):
+        """Count the number of frames and the amount of time spent when the mouse is in a segment"""
         if self.segments_df.empty:
             return
 
-        predictions = np.array(predictions)
         self.segments_df['elapsed_n_frames'] = self.segments_df.apply(
-            lambda segment: self._count_elapsed_n_frames(segment, predictions), axis=1)
+            lambda segment: sum(self._count_elapsed_n_frames(segment, predictions)), axis=1)
         self.segments_df['elapsed_sec'] = self.segments_df['elapsed_n_frames'].apply(
             lambda n_frames: n_frames/self.frames_per_second)
         self.segments_df['elapsed_sec%'] = self.segments_df['elapsed_n_frames'].apply(
             lambda n_frames: n_frames/self.num_frames*100)
 
-    def show_elapsed_time_in_segments(self, predictions):
-        """count elapsed time in each segment and plot bars"""
-        self.count_elapsed_time_in_segments(predictions)
-        st.dataframe(self.segments_df[["type", "elapsed_n_frames", "elapsed_sec", "elapsed_sec%"]])
+    def _plot_bars(self, x_col, height_col, title):
+        fig, ax = plt.subplots(figsize=(8, 6))
+        bars = plt.bar(self.segments_df[x_col], self.segments_df[height_col], color=["orange"])
 
-        fig, ax = plt.subplots(figsize=(8, 6), facecolor='blue')
-        ax.set_title("elapsed time percentage in segments", fontsize=16, color="#FFB266")
-        bars = plt.bar(self.segments_df["type"], self.segments_df["elapsed_sec%"], color=["orange"])
-
+        ax.set_title(title, fontsize=16, color="#FFB266", pad=20)
         # # get rid of the frame
         for spine in plt.gca().spines.values():
             spine.set_visible(False)
@@ -99,11 +95,32 @@ class Analyzer:
         # add value on top off the bar
         for b in bars:
             height = b.get_height()
-            plt.gca().text(b.get_x() + b.get_width() / 2, b.get_height() - 3, str(int(height)),
+            plt.gca().text(b.get_x() + b.get_width() / 2, b.get_height(), str(int(height)),
                            ha='center', color='white', fontsize=20)
         # remove ticks
-        ax.tick_params(top=False, bottom=False, left=False, right=False, labelleft=False, labelbottom=True, labelsize=12)
+        ax.tick_params(axis='x', colors='orange')
+        ax.tick_params(top=False, bottom=True, left=False, right=False, labelleft=False, labelbottom=True, labelsize=12)
 
         buf = BytesIO()
         fig.savefig(buf, format="png")
         st.image(buf)
+
+    def show_elapsed_time_in_segments(self, predictions):
+        """count elapsed time in each segment and plot bars"""
+
+        self._count_elapsed_time_in_segments(predictions)
+        st.dataframe(self.segments_df[["type", "elapsed_n_frames", "elapsed_sec", "elapsed_sec%"]])
+
+        self._plot_bars(x_col="type",
+                        height_col="elapsed_sec%",
+                        title="elapsed time percentage in segments")
+
+    def show_n_crossing_in_segments(self, predictions):
+        """count number of crossing in each segment and plot bars"""
+
+        self.segments_df['n_crossing'] = self.segments_df.apply(
+            lambda segment: sum(np.diff(self._count_elapsed_n_frames(segment, predictions))), axis=1)
+
+        self._plot_bars(x_col="type",
+                        height_col="n_crossing",
+                        title="number of crossings in segments")
