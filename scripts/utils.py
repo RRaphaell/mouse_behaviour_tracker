3cf0e7cb29adc6787e2df05a7eaaaadc064ef420
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-15 18:44:09 +0400

    use aggrid for interactive df, add color generator

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,8 @@ import pandas as pd
 import tempfile
 import streamlit as st
 from PIL import Image
+from st_aggrid import AgGrid    # for editable dataframe
+import matplotlib as plt
 
 
 def show_canvas_info(canvas_result):
@@ -28,8 +30,17 @@ def show_canvas_info(canvas_result):
             if objects.empty:
                 st.dataframe(objects)
             else:
-                st.dataframe(objects[["type", "left", "top", "width", "height", "scaleX", "scaleY", "angle"]])
+                # add segment key column which would be weights of each segment
+                objects.insert(loc=0, column='segment key', value=range(len(objects)))
 
+                # change dataframe configuration as editable only "segment key" column
+                visible_columns = ["type", "left", "top", "width", "height", "scaleX", "scaleY", "angle"]
+                grid_options = {"columnDefs": [{"field": "segment key", "editable": True}]}
+                grid_options["columnDefs"] += [{"field": c, "editable": False} for c in visible_columns]
+
+                # streamlit doesn't have interactive dataframe so using agrid dataframe.
+                grid_return = AgGrid(objects, grid_options)
+                objects = grid_return["data"]
     return objects
 
 
@@ -60,7 +71,28 @@ def read_video(file):
 
 
 def calculate_circle_center_cords(segment):
+    """calculate circle center based on radius, angle and corner coordinates using pythagorean theorem"""
     center_x = segment["left"] + segment["radius"] * np.cos(np.deg2rad(segment["angle"]))
     center_y = segment["top"] + segment["radius"] * np.sin(np.deg2rad(segment["angle"]))
     return center_x, center_y
+
+
+def rgba_0_1_to_0_255(color_palette):
+    color_palette = np.maximum(0, np.minimum(255, (color_palette * 256.0).astype(int)))
+    return color_palette
+
+
+def rgba_0_255_to_0_1(color_palette):
+    color_palette = np.array(list(color_palette))/255
+    return color_palette
+
+
+def generate_segments_colors(segments_df):
+    """generate different colors for each unique segments"""
+    color_palette = plt.cm.get_cmap("tab20", segments_df["segment key"].nunique()).colors  # color for each segment
+    color_palette = rgba_0_1_to_0_255(color_palette)
+    color_palette[:, -1] = 100  # add transparency
+    segment_colors = dict(zip(segments_df["segment key"].unique(), color_palette))
+
+    return segment_colors
 
