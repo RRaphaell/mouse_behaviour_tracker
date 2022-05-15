f20b32e03d2c763616089518f4fadce6e5e7ed05
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-15 18:42:51 +0400

    add layouts to center streamlit objects

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from scripts.Pipeline import Pipeline
-from scripts.config import SEGMENTS
+from scripts.config import SEGMENTS, CANVAS
 from scripts.utils import show_canvas_info, read_video
 
 
-st.set_page_config(page_title="Mouse behavior analysis")
+st.set_page_config(page_title="Mouse behavior analysis", layout="wide")
 st.markdown("<h1 style='text-align: center; color: #FF8000;'>Mouse behavior analysis </h1>", unsafe_allow_html=True)
 
 # # add styling
@@ -18,20 +18,25 @@ file = st.sidebar.file_uploader("Upload video:", type=["mp4"])
 
 video, first_image = read_video(file)
 
-# Create a canvas component
-canvas_result = st_canvas(
-    fill_color=SEGMENTS.fill_color,
-    stroke_width=SEGMENTS.stroke_width,
-    stroke_color=SEGMENTS.stroke_color,
-    background_image=first_image,
-    update_streamlit=True,
-    height=396,
-    width=704,
-    drawing_mode=drawing_mode,
-    key="canvas")
+_, center_layout, _ = st.columns([1, 3, 1])
+with center_layout:
+    # Create center_layout canvas component
+    canvas_result = st_canvas(
+        fill_color=SEGMENTS.fill_color,
+        stroke_width=SEGMENTS.stroke_width,
+        stroke_color=SEGMENTS.stroke_color,
+        background_image=first_image,
+        update_streamlit=True,
+        height=CANVAS.height,
+        width=CANVAS.width,
+        drawing_mode=drawing_mode,
+        key="canvas")
 
-start_btn = st.button("Start")
-objects = show_canvas_info(canvas_result)
+
+start_btn = center_layout.button("Start")
+with center_layout:
+    objects = show_canvas_info(canvas_result)
+
 if start_btn:
     if not video:
         st.warning("Please upload video first!")    # if user did not upload a video
