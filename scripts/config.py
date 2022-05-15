9063abb7a24e0943fa8161c1183649ef79ae0209
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-15 18:42:06 +0400

    add canvas parameters to config

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,6 @@
 class SEGMENTS:
     fill_color = "rgba(255, 165, 0, 0.3)",
-    stroke_width = 3
+    stroke_width = 2
     stroke_color = "red"
     fill = (255, 178, 102, 100)
 
@@ -9,3 +9,8 @@ class KEYPOINT:
     radius = 5
     fill = "red"
     outline = "red"
+
+
+class CANVAS:
+    width = 704
+    height = 396
