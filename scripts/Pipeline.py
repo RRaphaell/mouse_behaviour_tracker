3de32102e47eea8d113442f56e74c0219bbf9ab0
RRaphaell <raffo.kalandadze@gmail.com>
2022-05-21 22:25:22 +0400

    move video creating and while cycle into pipeline

--- scripts/Pipeline.py
+++ scripts/Pipeline.py
@@ -1,11 +1,15 @@
+import os
 import cv2
+import tempfile
 import numpy as np
 from scripts.Video import VideoStream
 from scripts.Tracker import Tracker
 from scripts.Analyzer import Analyzer
 from scripts.utils import generate_segments_colors
 from scripts.Report import Bar, Card, Dashboard, Pie
+import streamlit as st
 from types import SimpleNamespace
+
 from streamlit_elements import elements
 
 
@@ -21,7 +25,7 @@ class Pipeline:
         first_image (np.array): first image from video. Used as background for canvas and results placed on that also
     """
 
-    def __init__(self, video, segments_df, first_image):
+    def __init__(self, video, segments_df, first_image, file):
         """
         Initialize pipeline from video and segments information
 
@@ -32,37 +36,57 @@ class Pipeline:
         """
 
         segment_colors = generate_segments_colors(segments_df)
-
+        self.file = file
+        self.video = video
         self.report = SimpleNamespace(
             dashboard=Dashboard(),
-            n_crossing=Bar(0, 0, 6, 7, minW=3, minH=4),
-            time_spent=Pie(6, 0, 6, 7, minW=3, minH=4),
-            road_passed=Card(3, 7, 6, 7, minW=2, minH=4)
+            n_crossing=Bar(0, 0, 6, 8, minW=3, minH=4),
+            time_spent=Pie(6, 0, 6, 8, minW=3, minH=4),
+            road_passed=Card(3, 8, 6, 12, minW=2, minH=4)
         )
 
-        self.video_stream = VideoStream(video)
+        # self.video_stream = VideoStream(video)
         self.tracker = Tracker(segments_df, segment_colors)
         self.analyzer = Analyzer(video, segments_df, first_image, segment_colors, self.report)
         self.first_image = first_image
 
     def run(self):
         """this function runs video stream, use tracker to show segments, predictions and also analyzes them"""
-        self.video_stream.start()
-        while not self.video_stream.stopped():
-            frame = self.video_stream.read()
-            if frame is None:
+
+        # frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # fps = int(video.get(cv2.CAP_PROP_FPS))
+
+        file_out = tempfile.NamedTemporaryFile(suffix='.mp4')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(file_out.name, fourcc, 25.0, (256, 256))
+
+        while True:
+            success, img = self.video.read()
+            if not success:
                 break
 
-            self.tracker.draw_predictions(frame)
-            predictions = self.tracker.get_predictions()
+            img = self.tracker.draw_predictions(img)
+            out.write(np.array(img))
 
-        predictions = np.array(predictions)
+        self.video.release()
+        out.release()
+        cv2.destroyAllWindows()
 
+        os.system(f"ffmpeg -i {file_out.name} -c:v libx264 -c:a copy -f mp4 {file_out.name}_new")
+        video_file = open(f"{file_out.name}_new", "rb")
+
+        st.markdown("<h3 style='text-align: center; color: #FF8000;'>Video streaming</h3>", unsafe_allow_html=True)
+        st.video(video_file)
+
+        predictions = np.array(self.tracker.get_predictions())
+
+        st.markdown("<h3 style='text-align: center; color: #FF8000;'>Behavior report</h3>", unsafe_allow_html=True)
         with elements("demo"):
             with self.report.dashboard(rowHeight=57):
                 self.analyzer.draw_tracked_road(predictions)
                 self.analyzer.show_elapsed_time_in_segments(predictions)
                 self.analyzer.show_n_crossing_in_segments(predictions)
 
+        self.video.release()
         cv2.destroyAllWindows()
-        self.video_stream.stop()
