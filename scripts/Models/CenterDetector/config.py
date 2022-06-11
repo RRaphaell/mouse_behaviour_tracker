8066cddf22e45d9a6b20a327919b120be615c524
RRaphaell <raffo.kalandadze@gmail.com>
2022-06-11 17:33:01 +0400

    refactoring center detector model

--- scripts/Models/CenterDetector/config.py
+++ scripts/Models/CenterDetector/config.py
@@ -3,25 +3,34 @@ from pathlib import Path
 
 
 class CFG:
-    model_name       = 'Unet'
-    backbone         = 'resnet18'
-    img_size         = (512, 512)
-    decoder_channels = (256, 128, 64, 32)
-    encoder_depth    = 4
-    weights          = None
-    activation       = None
-    in_channels      = 1
-    num_classes      = 1
-    n_accumulate     = 1
-    train_bs         = 16
-    valid_bs         = 16
-    n_epoch          = 50
-    lr               = 0.001
+    model_name = 'Unet'
+    img_size = (512, 512)
 
-    device           = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    backbone = 'resnet18'
+    weights = 'imagenet'
+    decoder_channels = (256, 128, 64, 32, 16)
+    encoder_depth = 5
+    activation = None
 
-    sigma            = 40  # we use sigma for dot size when generating masks
-    model_out_dir    = "/scripts/Models/CenterDetector"
-    video_dir        = Path("/home/raphael/Desktop/mouse_data")
-    annotations_dir  = Path("/home/raphael/Desktop/mouse_data/annotations")
-    videos_name      = ["WIN_20201123_1-1"]
+    in_channels = 1
+    num_classes = 1
+
+    train_bs = 16
+    valid_bs = train_bs * 3
+    n_epoch = 50
+
+    lr = 0.001
+    scheduler = 'CosineAnnealingLR'
+    min_lr = 1e-6
+    T_max = int(400 / train_bs * n_epoch) + 50
+    T_0 = 25
+    warmup_epochs = 0
+    wd = 1e-6
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    sigma = 40  # we use sigma for dot size when generating masks
+    model_out_dir = "./"
+    video_dir = Path("/kaggle/input/mouse-video")
+    annotations_dir = Path("/kaggle/input/mouse-annotations")
+    videos_name = ["WIN_20201123_1-1"]
