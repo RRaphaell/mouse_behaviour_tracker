fda1a429b17a5e630fde649c11a2a395e4960812
RRaphaell <raffo.kalandadze@gmail.com>
2022-06-12 23:36:21 +0400

    change learning rate

--- scripts/Models/CenterDetector/config.py
+++ scripts/Models/CenterDetector/config.py
@@ -19,10 +19,10 @@ class CFG:
     valid_bs = train_bs * 3
     n_epoch = 50
 
-    lr = 0.001
+    lr = 0.003
     scheduler = 'CosineAnnealingLR'
     min_lr = 1e-6
-    T_max = int(400 / train_bs * n_epoch) + 50
+    T_max = int(200 / train_bs * n_epoch) + 50
     T_0 = 25
     warmup_epochs = 0
     wd = 1e-6
@@ -34,3 +34,4 @@ class CFG:
     video_dir = Path("/kaggle/input/mouse-video")
     annotations_dir = Path("/kaggle/input/mouse-annotations")
     videos_name = ["WIN_20201123_1-1"]
+
