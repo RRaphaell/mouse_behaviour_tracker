11680f81142f5b301cd6bc8d86b224eb638ce792
RRaphaell <raffo.kalandadze@gmail.com>
2022-07-12 18:45:46 +0400

    fix little bugs

--- scripts/Report/Card.py
+++ scripts/Report/Card.py
@@ -9,7 +9,7 @@ class Card(Dashboard.Item):
     def __call__(self, image):
 
         im = Image.fromarray(np.array(image))
-        im.save("/home/appuser/venv/bin/python/site-packages/streamlit_elements/frontend/build/temp.png")
+        im.save("/home/appuser/venv/lib/bin/python3.8/site-packages/streamlit_elements/frontend/build/temp.png")
 
         with mui.Card(key=self._key, sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"}, elevation=1):
             mui.CardHeader(
