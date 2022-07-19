cd0f46df2dfb614fb3573f7faee62c6d07ae69f2
RRaphaell <raffo.kalandadze@gmail.com>
2022-07-19 20:43:26 +0400

    add example and some information on UI

--- app.py
+++ app.py
@@ -16,18 +16,29 @@ def set_page_config():
 
 
 def main():
+    with st.sidebar:
+        st.info("""The project is in progress, we trained the model with a few images of rats,
+        so it would be inaccurate frequently, but we update it periodically.
+        If you have data that could be helpful, please contact us at raffo.kalandadze@gmail.com""")
+
     # Specify canvas parameters in application
-    drawing_mode = st.sidebar.selectbox("Drawing tool: 🖼", ("rect", "circle", "transform"))
+    drawing_mode = st.sidebar.selectbox("Drawing tool: 🖼", ("rect", "circle"))
 
     # create UI to uploading video
     file = st.sidebar.file_uploader("Upload video: 💾", type=["mp4"])
 
     with st.sidebar:
-        if not file:
-            st.warning("upload video \t ⚠️")
+        # example video option
+        example_btn = st.checkbox("use example", disabled=bool(file), value=not bool(file),
+                                  help="If you don't have a video, use our example")
+
+        if not file and not example_btn:
+            st.warning(" \t ⚠️ upload video file or use example")
+
         # About
         st.markdown(read_markdown("docs/about.md"), unsafe_allow_html=True)
 
+    file = open("examples/example.mp4", "rb") if example_btn else file
     video, video_params, first_image = read_video(file)
 
     # Create center_layout canvas component
@@ -48,8 +59,8 @@ def main():
     objects = show_canvas_info(canvas_result)
 
     if start_btn:
-        if not file:
-            st.warning("Please upload video first!")    # if the user did not upload a video
+        if not file and not example_btn:
+            st.warning(" \t ⚠️ Please upload video first or use example!")    # if the user did not upload a video
         elif objects.empty:
             st.warning("add at least one segment on canvas")  # if the user did not add the segment at all
         else:
