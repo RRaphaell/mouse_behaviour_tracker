b16de6254b625fdf5f453a8ffa1d27afa82678b3
RRaphaell <raffo.kalandadze@gmail.com>
2022-07-29 17:52:58 +0400

    change video name

--- scripts/utils.py
+++ scripts/utils.py
@@ -119,7 +119,7 @@ def create_video_output_file(frame_rate: float, height: int, width: int) -> Tupl
 
 
 def convert_mp4_standard_format(file_out: tempfile.NamedTemporaryFile):
-    os.system(f"ffmpeg -i -y {file_out.name} -c:v libx264 -c:a copy -f mp4 temp")
-    video_file = open(f"temp", "rb")
+    os.system(f"ffmpeg -i -y {file_out.name} -c:v libx264 -c:a copy -f mp4 generated_video")
+    video_file = open(f"generated_video", "rb")
     gc.collect()
     return video_file
