dad48591858be2708d6e99f6421533ae5a06ebea
RRaphaell <raffo.kalandadze@gmail.com>
2022-07-29 18:16:40 +0400

    debugging

--- scripts/utils.py
+++ scripts/utils.py
@@ -130,7 +130,7 @@ def list_files(startpath):
 
 def convert_mp4_standard_format(file_out: tempfile.NamedTemporaryFile):
     print(file_out.name)
-    os.system(f"ffmpeg -i {file_out.name} -c:v libx264 -c:a copy -f -y mp4 generated_video")
+    os.system(f"ffmpeg -i {file_out.name} -c:v libx264 -c:a copy -f mp4 -y generated_video")
     try:
         video_file = open("/tmp/generated_video.mp4", "rb")
     except:
