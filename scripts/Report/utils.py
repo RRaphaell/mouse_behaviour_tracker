99ebfc21fb2abc1e311159162deb6f61766ff6f0
RRaphaell <raffo.kalandadze@gmail.com>
2022-08-01 03:03:18 +0400

    for debugging

--- scripts/Report/utils.py
+++ scripts/Report/utils.py
@@ -20,6 +20,7 @@ def get_table_download_link(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     href = f'data:file/csv;base64,{b64}'
+    print(href)
     return href
 
 
